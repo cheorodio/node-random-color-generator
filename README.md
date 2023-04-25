23f136705fdb417885fb4992a22b58df71a1bfe2
cheorodio <cheorodio@gmail.com>
2023-04-25 21:14:10 +0200

    Modify index.js

--- README.md
+++ README.md
@@ -2,7 +2,29 @@
 
 Create an application that will return a random color in the command line.
 
+## Random color
+
 ```bash
 $ node index.js
 #c49206
+$ node index.js
+#c4063f
+```
+
+Accepts a request for hue
+
+```bash
+$ node index.js blue
+#06a2db
+$ node index.js red
+#c43936
+```
+
+Accepts a request for luminosity
+
+```bash
+$ node index.js blue light
+#bfcaff
+$ node index.js green dark
+#088e4d
 ```
--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
+
+//get color name that user specified on the command line
+const colorName = process.argv[2];
+//get color luminosity that user specified on the command line
+const colorLuminosity = process.argv[3];
+
+//this will generate the random color
+const colorInput = randomColor({
+  hue: `${colorName}`,
+  luminosity: `${colorLuminosity}`,
+});
+
+if (colorName) {
+  //if user didnt specify a color,
+  //display a random color
+  console.log(
+    chalk.hex(colorInput)(
+      `######################################
+######################################
+######################################
+#####                           ######
+#####         ${colorInput}           ######
+#####                           ######
+######################################
+######################################
+######################################`,
+    ),
+  );
+} else {
+  //if user specified a color or color + luminosity,
+  //display the specified color or color + luminosity
+  console.log(
+    chalk.hex(colorInput)(
+      `######################################
+######################################
+######################################
+#####                           ######
+#####         ${colorInput}           ######
+#####                           ######
+######################################
+######################################
+######################################`,
+    ),
+  );
+}
