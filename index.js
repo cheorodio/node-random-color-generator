9f7e8a804e8f505266320243b23419ea28133954
cheorodio <cheorodio@gmail.com>
2023-04-26 14:51:53 +0200

    Add comments in index.js after fixing my ESLint bug

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
+// get color name that user specified on the command line
 const colorName = process.argv[2];
+// get color luminosity that user specified on the command line
 const colorLuminosity = process.argv[3];
 
+// generate the random color
 const colorInput = randomColor({
   hue: colorName,
   luminosity: colorLuminosity,
