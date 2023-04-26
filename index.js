e34bccac94c2fe279b1a5dc32782e932d829f45e
cheorodio <cheorodio@gmail.com>
2023-04-26 11:19:14 +0200

    Modify index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-//get color name that user specified on the command line
 const colorName = process.argv[2];
-//get color luminosity in addition to color name that user specified on the command line
 const colorLuminosity = process.argv[3];
 
-//this will generate the random color
 const colorInput = randomColor({
   hue: colorName,
   luminosity: colorLuminosity,
 });
 
-if (!colorName) {
-  //if user didnt specify a color,
-  //display a random color
-  console.log(
-    chalk.hex(colorInput)(`
+console.log(
+  chalk.hex(colorInput)(`
 ############################
 ############################
 #####                 ######
@@ -24,18 +18,4 @@ if (!colorName) {
 #####                 ######
 ############################
 ############################`),
-  );
-} else if (colorName) {
-  //if user specified a color or color + luminosity,
-  //display the specified color or color + luminosity
-  console.log(
-    chalk.hex(colorInput)(`
-############################
-############################
-#####                 ######
-#####     ${colorInput}     ######
-#####                 ######
-############################
-############################`),
-  );
-}
+);
